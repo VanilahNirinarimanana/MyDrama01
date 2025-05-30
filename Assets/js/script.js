5aafb419ed956df3d5b1fcb9fa29be8660cf5ebc
Vanilah Nirinarimanana <vanilahnirinarimanana@gmail.com>
2025-05-30 20:45:24 +0200

    Ajout de mon projet de visionnage de drama sur github

---
+++ Assets/js/script.js
@@ -0,0 +1,8 @@
+const goToLogin = () =>  {
+    window.location.assign("http://127.0.0.1:5500/connexion.html")
+}
+
+function goToInscription() {
+    window.location.assign("http://127.0.0.1:5500/inscription.html")
+}
+
